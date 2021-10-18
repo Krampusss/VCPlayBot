7b75bc24f8528d1e46b1274fdc3cd06be210367a
Krampusss <90748072+Krampusss@users.noreply.github.com>
2021-10-18 18:06:02 +0530

    Update youtube.py

--- VCPlayBot/services/downloaders/youtube.py
+++ VCPlayBot/services/downloaders/youtube.py
@@ -1,7 +1,7 @@
 
 from os import path
 
-from youtube_dl import YoutubeDL
+from yt-dlp import YoutubeDL
 
 from VCPlayBot.config import DURATION_LIMIT
 from VCPlayBot.helpers.errors import DurationLimitError
